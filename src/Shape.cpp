29e166f5c109143ad5bb895a4338cc07795c60e2
Aaron Davis <aaron.m.davis@comcast.net>
2025-02-03 21:29:09 -0600

    added Shape class

---
+++ src/Shape.cpp
@@ -0,0 +1,7 @@
+// Shape.cpp : This file contains Shape Class implementation
+// Class demonstration for program 1
+// Author: Aaron Davis – This is my own work!
+// I understand that I may be asked questions about my submission.
+// email: amd0047@uah.edu
+// For: CS221-02, Spring 2025 Mrs. Delugach
+// Using compiler g++
